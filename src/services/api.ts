80852f0a4b19d7e0652e884c10fb1c99d98dbed1
Paulinha <paulafabianasoares@gmail.com>
2023-05-26 13:04:59 -0300

    Alteração da baseURL para https

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const ts = Number(new Date());
 const hash = md5(ts + import.meta.env.VITE_PRIVATE_KEY + import.meta.env.VITE_PUBLIC_KEY);
 
 const api = axios.create({
-    baseURL: 'http://gateway.marvel.com/v1/public',
+    baseURL: 'https://gateway.marvel.com/v1/public',
     params: {
       apikey: import.meta.env.VITE_PUBLIC_KEY,
       ts: ts,
